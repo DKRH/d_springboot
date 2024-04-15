251648717ef7dc9725545ec8e520c38f6d91b069
Dewangga <IT-Dewangga@SERVERLK2.LOCAL>
2024-04-15 10:53:51 +0700

    first

---
+++ src/main/java/com/example/tes3/Tes3Application.java
@@ -0,0 +1,13 @@
+package com.example.tes3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tes3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tes3Application.class, args);
+	}
+
+}
